796fca189a8804543c5bdbf290456a770ea782e4
akatranlp <fabian@nf-petersen.de>
2023-01-25 14:48:45 +0000

    Use own pandoc Docker-Image with some extra packages installed

--- thesis/create_thesis_pdf.sh
+++ thesis/create_thesis_pdf.sh
@@ -5,7 +5,7 @@ THESIS_FILE=./Bachelorthesis.pdf
 filenames=`ls | grep -E "[0-9][0-9].*.md"`
 
 docker run --rm --volume "/opt/gs-analysis/thesis:/data" --user `id -u`:`id -g` \
-pandoc/latex $filenames --defaults ./defaults.yaml
+akatranlp/pandoc $filenames --defaults ./defaults.yaml
 
 docker run --rm --volume "/opt/gs-analysis/thesis:/data" --user `id -u`:`id -g` \
 akatranlp/pdfhandler merge $DECKBLATT_FILE $RAW_THESIS_FILE $THESIS_FILE
